b60ccc2a4c2dcd8034e50496360552f22812cb4b
unknown <zxj@gato.com.cn>
2020-04-30 10:54:13 +0800

    commit webpack config

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default function play(callback) {
+  callback()
+}
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'yolanda-ui.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['js']
+  }
+};
